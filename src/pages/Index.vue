410d8a0bf04a293c06175d08778b3ddde082bc0a
Francesco Pastore <francesco0599@hotmail.it>
2020-09-04 21:00:57 +0200

    Basic todo page structure

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  <q-page padding>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <div>{{ task.name }}</div>
+        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
+    </ul>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'PageIndex',
+  data() {
+    return {
+      tasks: [
+        { name: 'Go to shop', dueDate: '2020/09/02', dueTime: '10:30' },
+        { name: 'Buy bananas', dueDate: '2020/09/01', dueTime: '12:30' },
+        { name: 'Buy apples', dueDate: '2020/09/04', dueTime: '18:30' },
+      ],
+    }
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    },
+  },
 }
 </script>
+
+<style></style>
