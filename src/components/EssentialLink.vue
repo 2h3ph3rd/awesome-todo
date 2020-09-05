77acad5cc1c2ed66405987295c3e8f731eb7c016
Francesco Pastore <francesco0599@hotmail.it>
2020-09-05 09:02:55 +0200

    Updated EssentialLink component structure

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,14 +1,18 @@
 <template>
-  <q-item clickable tag="a" target="_blank" :href="link">
-    <q-item-section v-if="icon" avatar>
-      <q-icon :name="icon" />
+  <q-item
+    clickable
+    :to="link.link"
+    exact
+  >
+    <q-item-section
+      v-if="link.icon"
+      avatar
+    >
+      <q-icon :name="link.icon" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
-        {{ caption }}
-      </q-item-label>
+      <q-item-label>{{ link.title }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
@@ -17,24 +21,9 @@
 export default {
   name: 'EssentialLink',
   props: {
-    title: {
-      type: String,
-      required: true,
-    },
-
-    caption: {
-      type: String,
-      default: '',
-    },
-
     link: {
-      type: String,
-      default: '#',
-    },
-
-    icon: {
-      type: String,
-      default: '',
+      type: Object,
+      required: true,
     },
   },
 }
