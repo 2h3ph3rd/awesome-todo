f718e2a9c171bbf9557f08e9e293c14f8b48f693
Francesco Pastore <francesco0599@hotmail.it>
2020-09-05 08:40:58 +0200

    Updated route to PageSettings

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/PageTodo.vue') },
-      { path: '', component: () => import('pages/PageSettings.vue') },
+      { path: '/settings', component: () => import('pages/PageSettings.vue') },
     ],
   },
 
