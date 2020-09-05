c914a8249cb7227bc08c2342a6dcb1edcb36dd20
Francesco Pastore <francesco0599@hotmail.it>
2020-09-05 09:02:37 +0200

    Added links to Settings and Todo pages

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,17 +24,19 @@
       content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Navigation
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
           :key="link.title"
-          v-bind="link"
+          :link="link"
         />
       </q-list>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -46,46 +48,14 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Todo',
+    icon: 'list',
+    link: '/',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    title: 'Settings',
+    icon: 'settings',
+    link: '/settings',
   },
 ]
 
