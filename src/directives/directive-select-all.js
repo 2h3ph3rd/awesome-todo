2bfe445537dd442813e1cf5a4f38958dcc34871c
Francesco Pastore <francesco0599@hotmail.it>
2020-09-29 17:29:17 +0200

    Removed console log in directive-select-all.js

--- src/directives/directive-select-all.js
+++ src/directives/directive-select-all.js
@@ -1,6 +1,5 @@
 export const selectAll = {
   inserted(el) {
-    console.log(el)
     let input = el.querySelector('.q-field__native')
     input.addEventListener('focus', () => {
       if (input.value.length) {
