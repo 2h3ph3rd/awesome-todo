bae9420f42ddb680efd8fe776d5537e4c9dbc990
Francesco Pastore <francesco0599@hotmail.it>
2020-10-25 19:42:41 +0100

    Updated dueTime accordingly with settings

--- src/components/Tasks/List/Shared/ListTask.vue
+++ src/components/Tasks/List/Shared/ListTask.vue
@@ -7,10 +7,7 @@
     v-touch-hold:1000.mouse="showEditTaskModal"
   >
     <q-item-section avatar>
-      <q-checkbox
-        :value="task.completed"
-        @input="onUpdateTask"
-      />
+      <q-checkbox :value="task.completed" @input="onUpdateTask" />
     </q-item-section>
     <q-item-section>
       <q-item-label
@@ -18,15 +15,12 @@
         v-html="$options.filters.searchHighlight(task.name, search)"
       ></q-item-label>
     </q-item-section>
-    <q-item-section
-      side
-      v-if="task.dueDate"
-    >
+    <q-item-section side v-if="task.dueDate">
       <q-icon name="event" />
     </q-item-section>
     <q-item-section side>
       <q-item-label>{{ task.dueDate | niceDate }}</q-item-label>
-      <q-item-label caption>{{ task.dueTime }}</q-item-label>
+      <q-item-label caption>{{ taskDueTime }}</q-item-label>
     </q-item-section>
     <q-item-section side>
       <q-btn
@@ -36,15 +30,8 @@
         icon="edit"
         @click.stop="showEditTaskDialog = true"
       />
-      <q-dialog
-        v-model="showEditTaskDialog"
-        persistent
-      >
-        <edit-task
-          :task="task"
-          :id="id"
-          @close="showEditTaskDialog = false"
-        />
+      <q-dialog v-model="showEditTaskDialog" persistent>
+        <edit-task :task="task" :id="id" @close="showEditTaskDialog = false" />
       </q-dialog>
     </q-item-section>
     <q-item-section side>
@@ -55,10 +42,7 @@
         icon="delete"
         @click.stop="showDeleteTaskDialog = true"
       />
-      <q-dialog
-        v-model="showDeleteTaskDialog"
-        persistent
-      >
+      <q-dialog v-model="showDeleteTaskDialog" persistent>
         <delete-task @delete="deleteTask(id)" />
       </q-dialog>
     </q-item-section>
@@ -66,7 +50,7 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapState, mapGetters } from 'vuex'
 import { Dialog, date } from 'quasar'
 const { formatDate } = date
 
@@ -76,7 +60,15 @@ export default {
     return { showEditTaskDialog: false, showDeleteTaskDialog: false }
   },
   props: ['task', 'id'],
-  computed: { ...mapState('tasks', ['search']) },
+  computed: {
+    ...mapState('tasks', ['search']),
+    ...mapGetters('settings', ['settings']),
+    taskDueTime() {
+      if (this.settings.show12HourTimeFormat)
+        return formatDate(this.task.dueDate + ' ' + this.task.dueTime, 'h:mm A')
+      return this.task.dueTime
+    }
+  },
   methods: {
     ...mapActions('tasks', ['updateTask', 'deleteTask']),
     onUpdateTask() {
@@ -97,11 +89,14 @@ export default {
   },
   filters: {
     niceDate(value) {
-      return formatDate(value, 'D/M')
+      return formatDate(value, 'MMM D')
     },
     searchHighlight(value, search) {
       if (search)
-        return value.replace(search, '<span class="bg-yellow-6">' + search + '</span>')
+        return value.replace(
+          search,
+          '<span class="bg-yellow-6">' + search + '</span>'
+        )
       return value
     }
   }
