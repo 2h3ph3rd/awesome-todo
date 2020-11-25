c945f5b23607d4995e332daa01deba0497e4cd9f
Francesco Pastore <francesco0599@hotmail.it>
2020-11-25 17:48:16 +0100

    Updated autofocus delay only for cordova app

--- src/directives/directive-autofocus.js
+++ src/directives/directive-autofocus.js
@@ -1,6 +1,9 @@
+import { Platform } from 'quasar'
+
 export const autofocus = {
   inserted(el) {
     let input = el.querySelector('.q-field__native')
-    setTimeout(() => input.focus(), 700)
+    let delay = Platform.is.cordova ? 700 : 0
+    setTimeout(() => input.focus(), delay)
   }
 }
